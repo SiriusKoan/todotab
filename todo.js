8c03cb73e7066026693b4b94d721118a0589c280
SiriusKoan <k.peihsun@gmail.com>
2023-03-27 19:42:36 +0800

    fix: make deadline not required

--- todo.js
+++ todo.js
@@ -5,10 +5,10 @@ todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const todoTitle = document.querySelector('#todo-title');
     const todoDeadline = document.querySelector('#todo-deadline');
-    chrome.storage.local.get(['todos'], function(result) {
+    chrome.storage.local.get(['todos'], function (result) {
         let todos = result.todos || [];
-        todos.push({"title": todoTitle.value, "deadline": todoDeadline.value});
-        chrome.storage.local.set({todos: todos}, function() {
+        todos.push({ "title": todoTitle.value, "deadline": todoDeadline.value });
+        chrome.storage.local.set({ todos: todos }, function () {
             console.log(`Todo ${todoTitle.value} saved`);
         });
         renderTodos(todoTitle.value, todoDeadline.value);
@@ -20,7 +20,7 @@ todoForm.addEventListener('submit', (e) => {
 
 
 window.onload = () => {
-    chrome.storage.local.get(['todos'], function(result) {
+    chrome.storage.local.get(['todos'], function (result) {
         let todos = result.todos || [];
         todos.forEach(todo => {
             renderTodos(todo.title, todo.deadline);
@@ -32,10 +32,10 @@ function deleteTodo(e) {
     const todoItem = e.target.parentElement;
     console.log(todoItem);
     const todoId = todoItem.todo_id;
-    chrome.storage.local.get(['todos'], function(result) {
+    chrome.storage.local.get(['todos'], function (result) {
         let todos = result.todos || [];
         todos.splice(todoId, 1);
-        chrome.storage.local.set({todos: todos}, function() {
+        chrome.storage.local.set({ todos: todos }, function () {
             console.log(`Todo ${todoId} deleted`);
             console.log(todoItem.parentElement);
             todoItem.parentElement.remove();
@@ -62,28 +62,30 @@ function renderTodos(title, deadline) {
     titleSpan.classList.add('todo-title');
     titleSpan.textContent = title;
     todoDiv.appendChild(titleSpan);
-    // deadline
-    const deadlineSpan = document.createElement('span');
-    deadlineSpan.classList.add('todo-deadline');
-    deadlineSpan.textContent = deadline;
-    // category
-    const today = new Date();
-    const deadlineDate = new Date(deadline);
-    const timeDiff = deadlineDate.getTime() - today.getTime();
-    const dayDiff = timeDiff / (1000 * 3600 * 24);
-    if (dayDiff < 0) {
-        deadlineSpan.classList.add('todo-overdue');
+    if (deadline) {
+        // deadline
+        const deadlineSpan = document.createElement('span');
+        deadlineSpan.classList.add('todo-deadline');
+        deadlineSpan.textContent = deadline;
+        // category
+        const today = new Date();
+        const deadlineDate = new Date(deadline);
+        const timeDiff = deadlineDate.getTime() - today.getTime();
+        const dayDiff = timeDiff / (1000 * 3600 * 24);
+        if (dayDiff < 0) {
+            deadlineSpan.classList.add('todo-overdue');
+        }
+        else if (dayDiff <= 1) {
+            deadlineSpan.classList.add('todo-urgent');
+        }
+        else if (dayDiff <= 3) {
+            deadlineSpan.classList.add('todo-warning');
+        }
+        else {
+            deadlineSpan.classList.add('todo-normal');
+        }
+        todoDiv.appendChild(deadlineSpan);
     }
-    else if (dayDiff <= 1) {
-        deadlineSpan.classList.add('todo-urgent');
-    }
-    else if (dayDiff <= 3) {
-        deadlineSpan.classList.add('todo-warning');
-    }
-    else {
-        deadlineSpan.classList.add('todo-normal');
-    }
-    todoDiv.appendChild(deadlineSpan);    
 
     todoContainer.appendChild(todoDiv);
     numberOfTodos++;
